b589883eb6d57f0337a8fd67aa163f7817544d3d
Gaby7077 <gamma@gmail.com>
2018-08-25 12:44:11 -0500

    friendsfinder

---
+++ data/friends.js
@@ -0,0 +1,57 @@
+var friendsArray = [
+    {
+        name:"Prince Harry",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9M5fWl0X7lctdPUp-2179f9JKUnkhzxLKQQVtx7P8py7b4RG67w",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+          ]
+    },
+    {
+      name:"Paul",
+      photo:"https://media1.popsugar-assets.com/files/thumbor/ZM_zsFc9SrsusDFrgmb5PQXU4u8/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/01/19/450/n/2589278/tmp_ZNjoDH_72c34c59cccc1f04_GettyImages-814057674.jpg",
+      scores:[
+          "3",
+          "2",
+          "1",
+          "5",
+          "4",
+          "3",
+          "1",
+          "2",
+          "5",
+          "1"
+        ]
+  },
+  {
+    name:"Han Solo",
+    photo:"http://www.iearobotics.com/wiki/images/d/d0/Han_solo.jpg",
+    scores:[
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "1",
+        "2",
+        "5",
+        "1"
+      ]
+}
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+
+
+module.exports = friendsArray;
+
+
